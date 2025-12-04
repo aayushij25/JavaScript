f6440edcc477187d97dbbe24d43ad14591a004f7
aayushij25 <aj25300jaiswal@gmail.com>
2025-12-04 21:41:12 +0530

    december 4 practice

---
+++ LetConstTempDeadZone.js
@@ -0,0 +1,13 @@
+console.log(b);
+// console.log(a); // Reference Error
+// console.log(x); // Reference Error
+let a; // Simply declaring a let var is fine
+// console.log(a);
+// let a = 200;
+var b = 100;
+// var a = 10; // Syntax error
+a = 10;
+console.log(a);
+// const c; // Syntax error
+const c = 1000; 
+// c = 4; // Type error
---
+++ UndefinedVsNotDefined.js
@@ -0,0 +1,18 @@
+// console.log(a);
+var a;
+console.log(a);
+// console.log(x);
+
+a = 10;
+console.log(a);
+a = "hello world";
+console.log(a);
+
+if(a === undefined) {
+    console.log('a is undefined');
+} else {
+    console.log('a is not undefined');
+}
+
+a = undefined; // bad practice
+console.log(a);
